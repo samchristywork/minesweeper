8f484512f295746f58f4f87b237d84967357e9bf
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-05 17:56:40 -0500

    Feature: Added code to do the actual rendering
    In addition to the code itself, which is pretty simple, I also need to
    include `SD2/SDL_ttf.h` in the header file to get it to compile.

--- src/render.cpp
+++ src/render.cpp
@@ -2,4 +2,17 @@
 
 void draw_text(SDL_Renderer *renderer, TTF_Font *font, int x, int y,
                SDL_Color color, const char *text, int square_size) {
+  if (!font) {
+    return;
+  }
+
+  SDL_Surface *textSurface = TTF_RenderText_Solid(font, text, color);
+  SDL_Texture *textTexture =
+      SDL_CreateTextureFromSurface(renderer, textSurface);
+  SDL_Rect rect = textSurface->clip_rect;
+  rect.x = x - rect.w / 2 + square_size / 2;
+  rect.y = y - rect.h / 2 + square_size / 2;
+  SDL_RenderCopy(renderer, textTexture, NULL, &rect);
+  SDL_FreeSurface(textSurface);
+  SDL_DestroyTexture(textTexture);
 }
--- src/render.h
+++ src/render.h
@@ -1,6 +1,8 @@
 #ifndef RENDER_H
 #define RENDER_H
 
+#include <SDL2/SDL_ttf.h>
+
 void draw_text(SDL_Renderer *renderer, TTF_Font *font, int x, int y,
                SDL_Color color, const char *text, int square_size);
 
